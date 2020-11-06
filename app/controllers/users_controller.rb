c461546984e85f205ce70e78e4aecedd3711cd74
k_akihiro <k.shuhari@gmail.com>
2020-11-06 23:56:29 +0900

    Createの動作の設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    # インスタンスを model から作成する
+    @user = User.new(
+      name: params[:name],
+      account: params[:account],
+      email: params[:email],
+    )
+
+    # インスタンスを DB に保存する
+    @user.save!
+    # json として値を返す
+    render :show
+  end
 end
